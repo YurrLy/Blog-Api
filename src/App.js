78daa65f3802d6706c48a234b21b552ce843f5de
Kalisson Yuri <kalisson.yuri99@gmail.com>
2023-08-22 11:58:27 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from "./img/OWNERLESS.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <nav className="flex">
+        <img className="w-60" src={Logo} alt="Logo" />
+      <div>
+        <a href="">SOBRE</a>
+        <a href="">CONTATOS</a>
+        <a href="">NOTICIAS</a>
+      </div>
+    </nav>
+    </header>
+    
   );
 }
 
