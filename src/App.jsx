446acc9152e0fd9181aa35cb1ba984b73f771959
Kalisson Yuri <kalisson.yuri99@gmail.com>
2023-08-31 22:46:55 -0300

    pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import Logo from "./img/OWNERLESS.png";
-import fundo from "./img/fundo.png";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [blog, setBlog] = useState({ title: "", content: "" }); // Fornecer valores iniciais
+  const [blog, setBlog] = useState([{ title: "", content: "" }]); 
 
   useEffect(() => {
-    fetch("https://api-rest-post-diegocandido.herokuapp.com/postagem/1")
+    fetch("https://api-rest-post-diegocandido.herokuapp.com/postagens")
       .then((res) => res.json())
       .then((json) => {
         setBlog(json)
@@ -16,32 +14,38 @@ function App() {
 
   return (
     <>
-    <header>
-      <nav className="flex justify-between my-5 ">
-        <img className="ml-10 w-60" src={Logo} alt="Logo" />
-      <div className="flex gap-20 mr-10 font-bold ">
-        <a className="text-2xl font-nav" href="">SOBRE</a>
-        <a className="text-2xl font-nav" href="">CONTATOS</a>
-        <a className="text-2xl font-nav" href="">NOTICIAS</a>
-      </div>
-    </nav>
-    </header>
-    <main>
-      <div className="relative flex justify-center">
-        <img className="px-10 absolute" src={fundo} alt="" />
-        <label className="absolute text-6xl text-white font-title font-black mt-40" >Mini Blog</label>
-        <label className="absolute text-4xl text-white font-title mt-72">Explorando ideias e inspirações em cada palavra.</label>
-      </div>
+      <header>
+        <nav className="flex justify-between my-5">
+          <img className="ml-10 w-60" src={"img/OWNERLESS.png"} alt="Logo" />
+          <div className="flex gap-20 mr-10 font-bold">
+            <a className="text-2xl font-nav" href="">SOBRE</a>
+            <a className="text-2xl font-nav" href="">CONTATOS</a>
+            <a className="text-2xl font-nav" href="">NOTICIAS</a>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div className="flex justify-center mb-24">
+          <img className="px-10 " src={"img/fundo.png"} alt="" />
+          <label className="absolute text-6xl text-white font-title font-black mt-40">Mini Blog</label>
+          <label className="absolute text-4xl text-white font-title mt-72">Explorando ideias e inspirações em cada palavra.</label>
+          <label className="absolute text-2xl text-white font-title mt-96">Confira noticas abaixo</label>
+        </div>
+      </main>
 
-      <section>
-      <h1 className="relative text-white text-xl font-semibold mt-2">{blog.title}</h1>
+      <section className="">
+      {blog.map((post, index) => (
+        <div key={index} className="noticias flex bg-cyan-950	m-10 border-4 border-black">
+          <img className="w-60" src={post.thumbImage} alt="" />
+          <div className="m-10">
+            <h1 className="text-justify font-title text-white text-xl font-bold">{post.title}</h1>
+            <p className="text-justify font-title text-white text-xl mt-10">{post.description}</p>
+          </div>
+        </div>
+      ))}
+    </section>
 
-      </section>
-
-    </main>
-  
     </>
-    
   );
 }
 
